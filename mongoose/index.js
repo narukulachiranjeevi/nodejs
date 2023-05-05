badfe072eedda88bc82e2677dfd16b4fb13a64d4
chiru <nchiru987@gmail.com>
2023-05-05 19:09:24 +0530

    added api

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+
 const app = express();
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
+
+app.get('/',(req,res) => {
+    res.send('hello world;');
+});
 mongoose.connect('mongodb://Localhost:27017/test',{useNewUrlParser: true, useUnifiedTopology: true}
 ,( err ) => {
     if (err) throw err;
